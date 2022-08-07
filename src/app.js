662488800cb4bab38776df692a3df14a83b95785
Manuelacostar97 <manuelacostar97@gmail.com>
2022-08-07 17:24:08 +0000

    Finalizacion

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  document.querySelector("#excuse").innerHTML = onLoad();
   console.log("Hello Rigo from the console!");
 };
+
+function onLoad() {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * who.length);
+  let whenIndex = Math.floor(Math.random() * who.length);
+
+  return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
+}
